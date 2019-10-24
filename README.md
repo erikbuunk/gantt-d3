e5752b70fd4d08c00b431b8c7f2da237b4e4502b
Erik Buunk <erik_buunk@iq.harvard.edu>
2019-10-24 10:30:17 -0400

    resize, UI changes

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ Run on server so data can be read
 This is is still a 0.1 version.
 
 ## TODOs
-- Show progress in bars
-- make size scalable when window is resized
-- SVG doesn't support text areas with automatic text wrapping
+- [ ] Show progress in bars
+- [ ] make size scalable when window is resized
+- [ ] SVG doesn't support text areas with automatic text wrapping
+- [ ] remove moment.js reference if it can be done in d3
+- [ ] make classes lower-case
+- [ ] interaction (resize columns)
+- [ ] tooltips
+- [ ] move/change/add/delete data in browser, save back the csv
+- [ ] Dependencies between (indicated with arrows)
+- [ ] Think of definitions: Track, Group
+- [ ] Add latest tick (Q3)
+- [ ] zoom out/in: weeks/months/Q's
--- scripts/charts.js
+++ scripts/charts.js
@@ -1,4 +1,3 @@
-
 let dataset; // global for datase
 
 // convert datatypes when importing new data
@@ -18,18 +17,49 @@ function rowConverter(d) {
 
 
 d3.csv('data/planning.csv', rowConverter)
-  .then(function(data) {
+  .then(function (data) {
     dataset = data;
     generateVis();
+    window.addEventListener('resize', function(event){
+      console.log('resized');
+      generateVis();
+    });
+    
   })
-  .catch(function(error) {
+  .catch(function (error) {
     console.log(error); // handle error
   })
 
 
+  /** TODO: Something like this could be used for multiline text */
+function multiLineText(text, x, y, width, height) {
+  return '<switch>\n' +
+  '<foreignObject x="' + x + '" y="' + y + '" width="' + width + '" height="' + height + '">\n' +
+    '<p xmlns="http://www.w3.org/1999/xhtml">' + text + '</p>\n' +
+    '</foreignObject>\n' +
+    '<text x="'+x+'" y="'+y+'">' + text + '</text>\n' +
+    '</switch>\n'
+}
+
 function generateVis() {
-  let w = 800; // widht of the svg area
-  let h = 500; // height of svg area
+  const body = document.body;
+  const html = document.documentElement;
+  // let w = 800; // widht of the svg area
+  // let h = 500; // height of svg area
+  
+  let h = parseInt(0.9* Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight ));
+  let w = parseInt(document.body.clientWidth *0.9);
+  
+  if (h<500) {
+    h=500;
+  }
+
+  if(w<800) {
+    w=800;
+  }
+  
+
   let n = Math.max.apply(Math, dataset.map(function(o) {
     return o.Line;
   })); // number of lines (Lines are numbered in the file because gantt block are sometimes placed on the same line)
@@ -40,8 +70,8 @@ function generateVis() {
   const wgroup = wtrack; // widht of the group column
   const htimeline = 75; // height of the axis
   const hTabTitles = 40; // height of the table header
-  const wgantt = w - wtrack - wgroup - 2 * padding - 2 * margin; // width of gantt chart
-  const hgantt = h - htimeline - padding - 2 * margin - hTabTitles; // height of gantt chart
+  // const wgantt = w - wtrack - wgroup - 2 * padding - 2 * margin; // width of gantt chart
+  // const hgantt = h - htimeline - padding - 2 * margin - hTabTitles; // height of gantt chart
   const hbar = parseInt((h - hTabTitles - htimeline - (n - 1) * padding) / n); // height of the bars
   const rounded = 0.3 * hbar; // rounded corners of bars
 
@@ -57,9 +87,6 @@ function generateVis() {
   var x = d3.scaleTime()
     .domain([startDate, endDate])
     .range([margin + wtrack + padding + wgroup + padding, w - margin])
-
-
-
   const x_axis = d3.axisBottom()
     .scale(x)
     .ticks(  5 )
@@ -89,29 +116,48 @@ function generateVis() {
 
 
   // Graphic elements
-  const svg = d3.select("body")
-    .append("svg")
+  const svg = d3.selectAll('svg')
     .attr("width", w)
     .attr("height", h);
 
+    // current date shown in verticl line
+    const now = parseInt(x(new Date()));
+
+    var lineGenerator = d3.line();
+    var points = [
+      [now, margin+hTabTitles/2-padding],
+      [now, h-htimeline/2 ]
+    ];
+    var pathData = lineGenerator(points);
+
+    d3.select("svg")
+      .append('path')
+      .attr('d', pathData)
+      .attr('class', 'now')
+      .attr("stroke-dasharray","10,10")
+      .attr("stroke-linecap", "round");
+
+
   //headers
   d3.select("svg")
-    .append("text")
+      .append("text")
       .text("Track")
       .attr("x", margin)
       .attr("y", hTabTitles/2 + margin)
-      .attr("class", "table-header");
+      .attr("class", "table-header")
+      .attr("id", 'track-header')
 
 
     d3.select("svg")
-      .append("text")
+       .append("text")
         .text("Group")
         .attr("x", margin+padding+wtrack)
         .attr("y", hTabTitles/2 + margin)
         .attr("class", "table-header")
+        .attr("id", 'group-header')
 
   // Gantt bars
-  const rect = svg.selectAll('rect')
+  svg.selectAll('rect')
     .data(dataset)
     .enter()
     .append('rect')
@@ -123,10 +169,10 @@ function generateVis() {
     .attr('width', d => x(d.End) - x(d.Start))
     .attr('height', hbar)
     .attr('class', d => d.Class)
-    .attr('rx', rounded + "px")
+    // .attr('rx', rounded + "px")
 
   // Labels in Gantt bars
-  const titles = svg.selectAll('#title')
+  svg.selectAll('#title')
     .data(dataset)
     .enter()
     .append('text')
@@ -136,7 +182,7 @@ function generateVis() {
     .attr('y', (d, i) => (d.Line - 1) * (hbar + padding) + margin + hbar / 2 +  hTabTitles)
     // .attr('width', wgantt/2)
     .text(d => d.Title)
-    .attr('class', 'bar-font')
+    .attr('class', d => d.Class +"-text bar-font")
 
 
   // Track Labels
@@ -182,21 +228,6 @@ function generateVis() {
       .attr("transform", "translate(0, " + (h-htimeline+margin+padding*2) + ")")
       .call(x_axis)
 
-    // current date shown in verticl line
-    const now = parseInt(x(new Date()));
 
-    var lineGenerator = d3.line();
-    var points = [
-      [now, margin+hTabTitles/2-padding],
-      [now, h-htimeline/2 ]
-    ];
-    var pathData = lineGenerator(points);
-
-    d3.select("svg")
-      .append('path')
-      .attr('d', pathData)
-      .attr('class', 'now')
-      .attr("stroke-dasharray","10,10")
-      .attr("stroke-linecap", "round");
 
 }
